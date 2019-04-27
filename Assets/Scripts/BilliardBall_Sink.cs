c10304b56a20c388074402d8007b856a7bdf1a26
PeraltaR0297 <48800841+PeraltaR0297@users.noreply.github.com>
2019-04-26 21:58:45 -0400

    Final Upload

---
+++ Assets/Scripts/BilliardBall_Sink.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BilliardBall_Sink : MonoBehaviour
+{
+    public float SinkSpeed;
+    public GameObject BilliardBall;
+    private Rigidbody BallCol;
+    private float BallSpeed;
+
+    void Awake()
+    {
+
+        BallCol = GetComponent<Rigidbody>();
+        Vector3 Velocity = BallCol.velocity;
+        BallSpeed = BallCol.velocity.magnitude;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Pockets" && BallSpeed < SinkSpeed)
+        {
+            Destroy(BilliardBall);
+        }
+    }
+
+}
